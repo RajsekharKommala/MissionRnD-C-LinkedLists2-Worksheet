a4ec7a3669b7a68bfef6a7379357145ae3d11b2a
RajsekharKommala <RajsekharKommala@missionrnd.com>
2016-03-30 18:48:07 -0700

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -12,53 +12,117 @@ NOTES:
 */
 
 #include <stdio.h>
-
+#include <stdlib.h>
 struct node {
 	int num;
 	struct node *next;
 };
 
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	node *mergedList, *output;
 	if (head1 == NULL && head2 == NULL)
 		return NULL;
-	if (head1==NULL)
-	return head1;
-	if (head2 == NULL)
-		return head2;
-	if (head1->num < head2->num)
+	else
 	{
-		mergedList = head1;
-		output = mergedList;
-		head1 = head1->next;
-	}
-	else{
-		mergedList = head2;
-		output = mergedList;
-		head2 = head2->next;
-	}
-	while (head1 != NULL && head2 != NULL)
-	{
-		if (head1->num < head2->num)
+		struct node *output,*newnode,*temp;
+		int i = 0;
+		output = (struct node*)malloc(sizeof(struct node));
+
+		if (head1 == NULL)
 		{
-			mergedList->next = head1;
-			mergedList = mergedList->next;
-			head1 = head1->next;
+			return head2;
 		}
 		else
+			if (head2 == NULL)
+			{
+				return head1;
+			}
+
+		while (head1 != NULL || head2 != NULL)
 		{
-			mergedList->next = head2;
-			mergedList = mergedList->next;
-			head2 = head2->next;
-		}
-		if (head1 = NULL)
-		{
-			mergedList->next = head2;
-		}
-		else
-		{
-			mergedList->next = head1;
+			if (head1 == NULL || head2 == NULL)
+			{
+				if (head1 == NULL)
+				{
+					newnode = (struct node*)malloc(sizeof(struct node));
+					newnode->num = head2->num;
+					newnode->next = NULL;
+					head2 = head2->next;
+					if (i == 0)
+					{
+						 temp = newnode;
+						output = temp;
+						i = 1;
+					}
+					else
+					{
+						temp->next = newnode;
+						temp = temp->next;
+					}
+
+				}
+				else
+					if (head2 == NULL)
+					{
+						newnode = (struct node*)malloc(sizeof(struct node));
+						newnode->num = head1->num;
+						newnode->next = NULL;
+						head1 = head1->next;
+						if (i == 0)
+						{
+							temp = newnode;
+							output = temp;
+							i = 1;
+						}
+						else
+						{
+							temp->next = newnode;
+							temp = newnode;
+						}
+					}
+			}
+			else
+			{
+				if (head1->num < head2->num)
+				{
+					newnode = (struct node*)malloc(sizeof(struct node));
+					newnode->num = head1->num;
+					newnode->next = NULL;
+					head1 = head1->next;
+					if (i == 0)
+					{
+						temp = newnode;
+						output = temp;
+						i = 1;
+					}
+					else
+					{
+						temp->next = newnode;
+						temp = newnode;
+					}
+				}
+				else
+				{
+					newnode = (struct node*)malloc(sizeof(struct node));
+					newnode->num = head2->num;
+					newnode->next = NULL;
+					head2 = head2->next;
+					if (i == 0)
+					{
+						temp = newnode;
+						output = temp;
+						i = 1;
+					}
+					else
+					{
+						temp->next = newnode;
+						temp = temp->next;
+					}
+
+				}
+
+			}
 		}
 		return output;
 	}
+	return NULL;
 }
